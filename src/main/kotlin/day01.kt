1540d6a57f641cda1f57abf3c7bdb8cda57b1633
Thomas Kint <thomaskint.pro@gmail.com>
2020-12-07 14:47:11 +0100

    Initial commit - day 1

---
+++ src/main/kotlin/day01.kt
@@ -0,0 +1,42 @@
+import java.io.File
+
+fun main() {
+    File("input/day01.txt").readLines().let { lines ->
+        val data = lines.map { it.toInt() }
+
+        val sumOf2 = findSumOf2(data, 2020)
+        val sumOf3 = findSumOf3(data, 2020)
+
+        println(sumOf2)
+        println(sumOf3)
+    }
+}
+
+fun findSumOf2(data: List<Int>, sum: Int): Int? {
+    var value: Int? = null
+    var i = 0
+    while (i < data.size && value == null) {
+        var j = i
+        while (j < data.size && value == null) {
+            if (data[i] + data[j] == sum) {
+                value = data[i] * data[j]
+            }
+            j++
+        }
+        i++
+    }
+    return value
+}
+
+fun findSumOf3(data: List<Int>, sum: Int): Int? {
+    var value: Int? = null
+    var i = 0
+    while (i < data.size && value == null) {
+        val subSum = findSumOf2(data, sum - data[i])
+        if (subSum != null) {
+            value = data[i] * subSum
+        }
+        i++
+    }
+    return value
+}
