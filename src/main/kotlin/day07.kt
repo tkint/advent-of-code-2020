9361f2beabc84e6988d69b15dd1d137069878c66
Thomas Kint <thomaskint.pro@gmail.com>
2020-12-20 18:27:19 +0100

    feat: day 7 first part

---
+++ src/main/kotlin/day07.kt
@@ -0,0 +1,76 @@
+import java.lang.Exception
+
+@ExperimentalStdlibApi
+fun main() {
+    withInputData("day07.txt") { data ->
+        val rules = data.map { Day07.Rule.fromString(it) }
+
+        val rulesWithShinyGold = rules.flatMap { rule ->
+            Day07.bagsWithAtLeastOne(rule, "shiny gold", rules)
+        }.toSet()
+
+        println(rulesWithShinyGold.size)
+    }
+}
+
+@ExperimentalStdlibApi
+object Day07 {
+    data class Rule(
+        val color: String,
+        val children: Map<String, Int>,
+    ) {
+        companion object {
+            private val ruleRegex = Regex("(.*) bags contain (.*)")
+            private val containsRegex = Regex("([0-9]*) (.*) bag.*")
+
+            fun fromString(str: String): Rule {
+                return ruleRegex.find(str)?.let {
+                    val (color, contains) = it.destructured
+                    Rule(
+                        color = color,
+                        children = strToChildren(contains),
+                    )
+                } ?: throw Exception("Rule $str not valid")
+            }
+
+            private fun strToChildren(str: String): Map<String, Int> {
+                return if (str.contains("no other bags")) mapOf()
+                else {
+                    str.split(",").map { s ->
+                        containsRegex.find(s.trim())?.let {
+                            val (nb, color) = it.destructured
+                            color to nb.toInt()
+                        } ?: throw Exception("Contains $str not valid")
+                    }.toMap()
+                }
+            }
+        }
+    }
+
+    fun bagsWithAtLeastOne(rule: Rule, color: String, rules: List<Rule>): Set<String> {
+        val bags = mutableSetOf<String>()
+
+        rule.children.forEach { (childColor, _) ->
+            if (childColor == color) {
+                bags.add(rule.color)
+            }
+
+            val childRule = rules.find { it.color == childColor }
+                ?: throw Exception("Rule $childColor not found")
+
+            if (childRule.children.containsKey(color)) {
+                bags.add(rule.color)
+                bags.add(childColor)
+            }
+
+            val subBags = bagsWithAtLeastOne(childRule, color, rules)
+            if (subBags.isNotEmpty()) {
+                bags.add(rule.color)
+                bags.add(childColor)
+                bags.plus(subBags)
+            }
+        }
+
+        return bags
+    }
+}
